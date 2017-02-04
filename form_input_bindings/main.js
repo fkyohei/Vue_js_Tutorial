7720311300860e1df41a9b7fd6095452893bce88
fkyohei <kyohei19921016@gmail.com>
2017-02-04 17:19:45 +0900

    day 12

---
+++ form_input_bindings/main.js
@@ -0,0 +1,81 @@
+var example1 = new Vue({
+    el: '#example-1',
+    data: {
+        message: ''
+    }
+})
+
+var example2 = new Vue({
+    el: '#example-2',
+    data: {
+        message: ''
+    }
+})
+
+var example3 = new Vue({
+    el: '#example-3',
+    data: {
+        checked: false
+    }
+})
+
+var example4 = new Vue({
+    el: '#example-4',
+    data: {
+        checkedNames: []
+    }
+})
+
+var example5 = new Vue({
+    el: '#example-5',
+    data: {
+        picked: ''
+    }
+})
+
+var example6 = new Vue({
+    el: '#example-6',
+    data: {
+        selected: null
+    }
+})
+
+var example7 = new Vue({
+    el: '#example-7',
+    data: {
+        selected: []
+    }
+})
+
+var example8 = new Vue({
+    el: '#example-8',
+    data: {
+        selected: 'A',
+        options: [
+            { text: 'One', value: 'A' },
+            { text: 'Two', value: 'B' },
+            { text: 'Three', value: 'C' }
+        ]
+    }
+})
+
+var example9 = new Vue({
+    el: '#example-9',
+    data: {
+        msg: ''
+    }
+})
+
+var example10 = new Vue({
+    el: '#example-10',
+    data: {
+        age: ''
+    }
+})
+
+var example11 = new Vue({
+    el: '#example-11',
+    data: {
+        msg: ''
+    }
+})
