ee4b969fc9566e0703d8b5aa44f25ed00e581e37
fkyohei <kyohei19921016@gmail.com>
2017-02-08 04:00:11 +0900

    day 15

--- components/main.js
+++ components/main.js
@@ -112,3 +112,135 @@ Vue.component('example', {
 })
 // 上記バリデーションで使用できる型は、String, Number, Boolean, Function, Object, Array
 
+Vue.component('button-counter', {
+    // v-on:clickでincrementメソッドを実行するようボタンを生成させる
+    template: '<button v-on:click="increment">{{ counter }}</button>',
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        increment: function() {
+            this.counter += 1
+            // 親コンポーネント(button-counter)に対してincrementというイベントを発行する（v-on:incrementが呼ばれる）
+            this.$emit('increment')
+        }
+    }
+})
+new Vue({
+    el: '#counter-event-example',
+    data: {
+        total: 0
+    },
+    methods: {
+        incrementTotal: function() {
+            this.total += 1
+        }
+    }
+})
+
+Vue.component('currency-input', {
+    template: '\
+        <span>\
+            $\
+            <input\
+                ref="input"\
+                v-bind:value="value"\
+                v-on:input="updateValue($event.target.value)"\
+            >\
+        </span>\
+    ',
+    props: ['value'],
+    methods: {
+        updateValue: function(value) {
+            var formattedValue = value
+                .trim()
+                .slice(0, value.indexOf('.') + 3)
+
+            if(formattedValue !== value) {
+                this.$refs.input.value = formattedValue
+            }
+            this.$emit('input', Number(formattedValue))
+        }
+    }
+})
+new Vue({
+    el: '#form-example',
+    data: {
+        price: ''
+    }
+})
+
+// 実例
+Vue.component('currency-input2', {
+    template: '\
+        <div>\
+            <label v-if="label">{{ label }}</label>\
+            $\
+            <input\
+                ref="input"\
+                v-bind:value="value"\
+                v-on:input="updateValue($event.target.value)"\
+                v-on:focus="selectAll"\
+                v-on:blur="formatValue"\
+            >\
+        </div>\
+    ',
+    props: {
+        value: {
+            type: Number,
+            default: 0
+        },
+        label: {
+            type: String,
+            default: ''
+        }
+    },
+    mounted: function() {
+        this.formatValue()
+    },
+    methods: {
+        updateValue: function(value) {
+            var result = currencyValidator.parse(value, this.value)
+            if(result.warning) {
+                this.$refs.input.value = result.value
+            }
+            this.$emit('input', result.value)
+        },
+        formatValue: function() {
+            this.$refs.input.value = currencyValidator.format(this.value)
+        },
+        selectAll: function(event) {
+            setTimeout(function() {
+                event.target.select()
+            }, 0)
+        }
+    }
+})
+new Vue({
+    el: '#app',
+    data: {
+        price: 0,
+        shipping: 0,
+        handling: 0,
+        discount: 0
+    },
+    computed: {
+        total: function() {
+            return ((
+                this.price * 100 +
+                this.shipping * 100 +
+                this.handling * 100 -
+                this.discount * 100
+            ) / 100).toFixed(2)
+        }
+    }
+})
+
+// $emit, $onは親子でなくても良い
+var bus = new Vue()
+bus.$emit('id-selected', 1)
+bus.$on('id-selected', function(id) {
+    // hogehoge
+})
